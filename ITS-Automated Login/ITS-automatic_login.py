a74c459a88ea427219d0f2dba871c6bfeda647d0
ankit16-19 <ankitsocial16@gmail.com>
2017-02-03 11:56:56 +0530

    Create ITS-automatic_login.py

---
+++ ITS-Automated Login/ITS-automatic_login.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import requests
+import time
+
+
+login_req = requests.get("http://www.iiit-bh.ac.in")
+login_req_url = login_req.url
+magic_token = login_req_url[32:]
+
+if(login_req_url!="http://www.iiit-bh.ac.in/"):
+	with requests.session() as s:
+		login_url = "http://172.16.1.11:1000/"
+		referer = login_req_url
+
+		login_form = {
+		"4Tredir":"http://www.iiit-bh.ac.in/",
+		"magic":magic_token,
+		"username":"+xxxxx",
+		"password":"+xxxxx"
+		}
+		login = s.post(url = login_url,data = login_form,headers={'referer':referer})
+		if(login.['content-length']!=1559)
+			print "success" + " " +  time.strftime("%I:%M:%S") + " " + time.strftime("%d/%m/%Y")
+
